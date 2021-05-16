1be139bb540140951d86e866790382da4e0bb9b0
1425506115@qq.com <1425506115@qq.com>
2021-05-16 23:20:49 +0800

    V0.1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,146 @@ namespace LOL_Little_Book
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.action = new System.Windows.Forms.CheckedListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.name = new System.Windows.Forms.ComboBox();
+            this.team = new System.Windows.Forms.Label();
+            this.state = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.hero = new System.Windows.Forms.Label();
+            this.info = new System.Windows.Forms.TextBox();
+            this.add = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "游戏状态：";
+            // 
+            // action
+            // 
+            this.action.FormattingEnabled = true;
+            this.action.Location = new System.Drawing.Point(485, 20);
+            this.action.Name = "action";
+            this.action.Size = new System.Drawing.Size(221, 164);
+            this.action.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(219, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "作为";
+            // 
+            // name
+            // 
+            this.name.FormattingEnabled = true;
+            this.name.Location = new System.Drawing.Point(28, 102);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(169, 23);
+            this.name.TabIndex = 4;
+            this.name.SelectedIndexChanged += new System.EventHandler(this.name_SelectedIndexChanged);
+            // 
+            // team
+            // 
+            this.team.AutoSize = true;
+            this.team.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.team.Location = new System.Drawing.Point(262, 105);
+            this.team.Name = "team";
+            this.team.Size = new System.Drawing.Size(52, 15);
+            this.team.TabIndex = 5;
+            this.team.Text = "猪队友";
+            // 
+            // state
+            // 
+            this.state.AutoSize = true;
+            this.state.ForeColor = System.Drawing.Color.Tomato;
+            this.state.Location = new System.Drawing.Point(123, 20);
+            this.state.Name = "state";
+            this.state.Size = new System.Drawing.Size(52, 15);
+            this.state.TabIndex = 6;
+            this.state.Text = "未进行";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(320, 105);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(22, 15);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "玩";
+            // 
+            // hero
+            // 
+            this.hero.AutoSize = true;
+            this.hero.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.hero.Location = new System.Drawing.Point(359, 105);
+            this.hero.Name = "hero";
+            this.hero.Size = new System.Drawing.Size(0, 15);
+            this.hero.TabIndex = 8;
+            // 
+            // info
+            // 
+            this.info.Location = new System.Drawing.Point(76, 225);
+            this.info.Multiline = true;
+            this.info.Name = "info";
+            this.info.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.info.Size = new System.Drawing.Size(630, 128);
+            this.info.TabIndex = 9;
+            // 
+            // add
+            // 
+            this.add.Location = new System.Drawing.Point(239, 161);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(137, 41);
+            this.add.TabIndex = 10;
+            this.add.Text = "添加";
+            this.add.UseCompatibleTextRendering = true;
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(755, 365);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.info);
+            this.Controls.Add(this.hero);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.state);
+            this.Controls.Add(this.team);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.action);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "LOL Little Book";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckedListBox action;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox name;
+        private System.Windows.Forms.Label team;
+        private System.Windows.Forms.Label state;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label hero;
+        private System.Windows.Forms.TextBox info;
+        private System.Windows.Forms.Button add;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -6,14 +6,157 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using System.Threading;
+using System.Net;
+using Newtonsoft.Json;
 
 namespace LOL_Little_Book
 {
     public partial class Form1 : Form
     {
+        string gamestats = "https://127.0.0.1:2999/liveclientdata/gamestats";
+        string playerlist = "https://127.0.0.1:2999/liveclientdata/playerlist";
+        JArray player;
+        int flag = 0;
         public Form1()
         {
             InitializeComponent();
+        }
+  
+        private void Form1_Load(object sender, EventArgs e)
+        { 
+
+                using (StreamReader sr = new StreamReader("./Data/action.txt"))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        action.Items.Add(line);
+                    }
+                }
+            Thread th = new Thread(run);
+            th.IsBackground = true;
+            th.Start();
+        }
+        void run()
+        {
+            while (true)
+            {
+                if (flag == 0)
+                {
+                    if (httpsreq(gamestats) != "")
+                        flag = 1;
+                }
+                else if (flag==1)
+                {
+                    player = (JArray)JsonConvert.DeserializeObject(httpsreq(playerlist));
+                    for (int i=0;i< player.Count; i++)
+                    {
+                        string _name = player[i]["summonerName"].ToString();
+                        string text = File.ReadAllText("Data\\AllUser.txt");
+                        if(text.Contains(_name + " 作为"))
+                        {
+                            string oldline="",line;
+                            StreamReader file =new StreamReader("Data\\AllUser.txt");
+                            while ((line = file.ReadLine()) != null)
+                            {
+                                if(line.Contains(_name + " 作为"))
+                                {
+                                    oldline = line;
+                                    string re = line.Substring(line.Length - 9, 9);
+                                    int count=int.Parse(System.Text.RegularExpressions.Regex.Replace(re, @"[^0-9]+", ""));
+                                    line=line.Replace(re, re.Replace(count.ToString(), (count + 1).ToString()));
+                                    
+                                    break;
+                                }
+                            }
+                            file.Close();
+                            if (line != "")
+                                text.Replace(oldline, line);
+                            info.Text = line + "\r\n" + info.Text;
+                        }
+                        name.Items.Add(_name);
+                    }
+                    state.Text = "正在游戏中";
+                    flag = 2;
+                }
+                else
+                {
+                    if (httpsreq(gamestats) != "")
+                        Thread.Sleep(15000);
+                    else
+                    {
+                        state.Text = "未进行";
+                        name.Items.Clear();
+                        flag = 0;
+                    }
+                }
+                Thread.Sleep(1000);
+            }
+        }
+        string httpsreq(string url)
+        {
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
+                ServicePointManager.ServerCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true;
+                request.Method = "GET";
+                request.Timeout = 3000;
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                return (new StreamReader(response.GetResponseStream())).ReadToEnd();
+            }
+            catch (WebException ex)
+            {
+                return "";
+            }
+        }
+
+        private void name_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            hero.Text = player[name.SelectedIndex]["championName"].ToString();
+            if (name.SelectedIndex <= 4)
+                team.Text = "猪队友";
+            else
+                team.Text = "狗对手";
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            string text = File.ReadAllText("Data\\AllUser.txt");
+            string _name = name.SelectedItem.ToString();
+            if (_name == "") return;
+            if (!text.Contains(_name + " 作为")) 
+            { 
+                File.AppendAllText("Data\\AllUser.txt", DateTime.Now.ToShortDateString().ToString()+":"+_name + " 作为 "+ team.Text+"玩 "+hero.Text+ selecta() + " 这是第1次遇到\r\n");
+                info.Text = _name + "添加成功！\r\n" + info.Text;
+                //string line = name.Text + " 作为 " + team.Text + "玩 " + hero.Text + selecta() + " 这是第1次遇到";
+                //string re = line.Substring(line.Length - 9, 9);
+                //int count = int.Parse(System.Text.RegularExpressions.Regex.Replace(re, @"[^0-9]+", ""));
+                //line = line.Replace(re, re.Replace(count.ToString(), (count + 1).ToString()));
+                //info.Text += line;
+            }
+            else
+            {
+                info.Text = "此人在小本本里了，可恶，真是带恶人\r\n" + info.Text;
+            }
+        }
+        string selecta()
+        {
+            string res = "";
+            bool flag = false;
+            for(int i=0;i< action.Items.Count; i++)
+            {
+                if (action.GetItemChecked(i))
+                {
+                    string rv = action.GetItemText(action.Items[i]);
+                    res +=(flag?"还":" ")+rv;
+                    flag = true;
+                }
+            }
+            return res;
         }
     }
 }
